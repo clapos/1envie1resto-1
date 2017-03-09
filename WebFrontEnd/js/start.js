2eb0fae7e697e74decc3b5a4f34776c16544d4b1
Rapx3 <elfar.nop@gmail.com>
2017-03-09 16:56:21 +0100

    Mise à jour du jeudi 9 mars

---
+++ WebFrontEnd/js/start.js
@@ -0,0 +1,6 @@
+ $('#sandbox-container input').datepicker({
+                                maxViewMode: 1,
+                                todayBtn: "linked",
+                                language: "fr",
+                                keyboardNavigation: false
+                            });
